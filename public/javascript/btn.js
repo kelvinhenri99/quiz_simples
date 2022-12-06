7d99e76039be4530d31aa7ab5ef4d268981e1a39
kelvinhenri99 <kelvin5henri@gmail.com>
2022-12-06 15:53:32 -0300

    Implementando o sistema na versao 1.0

---
+++ public/javascript/btn.js
@@ -0,0 +1,229 @@
+const btn1 = document.getElementById("p1r1");
+const btn2 = document.getElementById("p1r2");
+const btn3 = document.getElementById("p1r3");
+const btn4 = document.getElementById("p1r4");
+
+const btn5 = document.getElementById("p2r1");
+const btn6 = document.getElementById("p2r2");
+const btn7 = document.getElementById("p2r3");
+const btn8 = document.getElementById("p2r4");
+
+const btn9  = document.getElementById("p3r1");
+const btn10 = document.getElementById("p3r2");
+const btn11 = document.getElementById("p3r3");
+const btn12 = document.getElementById("p3r4");
+
+const btn13 = document.getElementById("p4r1");
+const btn14 = document.getElementById("p4r2");
+const btn15 = document.getElementById("p4r3");
+const btn16 = document.getElementById("p4r4");
+
+const btn17 = document.getElementById("p5r1");
+const btn18 = document.getElementById("p5r2");
+const btn19 = document.getElementById("p5r3");
+const btn20 = document.getElementById("p5r4");
+
+btn1.onclick = function() {
+    btn1.style.backgroundColor = "steelblue";
+    btn1.style.color = "white";
+    btn2.style.backgroundColor = "transparent";
+    btn2.style.color = "rgb(40,40,40)";
+    btn3.style.backgroundColor = "transparent";
+    btn3.style.color = "rgb(40,40,40)";
+    btn4.style.backgroundColor = "transparent";
+    btn4.style.color = "rgb(40,40,40)";
+}
+btn2.onclick = function() {
+    btn1.style.backgroundColor = "transparent";
+    btn1.style.color = "rgb(40,40,40)";
+    btn2.style.backgroundColor = "steelblue";
+    btn2.style.color = "white";
+    btn3.style.backgroundColor = "transparent";
+    btn3.style.color = "rgb(40,40,40)";
+    btn4.style.backgroundColor = "transparent";
+    btn4.style.color = "rgb(40,40,40)";
+}
+btn3.onclick = function() {
+    btn1.style.backgroundColor = "transparent";
+    btn1.style.color = "rgb(40,40,40)";
+    btn2.style.backgroundColor = "transparent";
+    btn2.style.color = "rgb(40,40,40)";
+    btn3.style.backgroundColor = "steelblue";
+    btn3.style.color = "white";
+    btn4.style.backgroundColor = "transparent";
+    btn4.style.color = "rgb(40,40,40)";
+}
+btn4.onclick = function() {
+    btn1.style.backgroundColor = "transparent";
+    btn1.style.color = "rgb(40,40,40)";
+    btn2.style.backgroundColor = "transparent";
+    btn2.style.color = "rgb(40,40,40)";
+    btn3.style.backgroundColor = "transparent";
+    btn3.style.color = "rgb(40,40,40)";
+    btn4.style.backgroundColor = "steelblue";
+    btn4.style.color = "white";
+}
+
+btn5.onclick = function() {
+    btn5.style.backgroundColor = "steelblue";
+    btn5.style.color = "white";
+    btn6.style.backgroundColor = "transparent";
+    btn6.style.color = "rgb(40,40,40)";
+    btn7.style.backgroundColor = "transparent";
+    btn7.style.color = "rgb(40,40,40)";
+    btn8.style.backgroundColor = "transparent";
+    btn8.style.color = "rgb(40,40,40)";
+}
+btn6.onclick = function() {
+    btn5.style.backgroundColor = "transparent";
+    btn5.style.color = "rgb(40,40,40)";
+    btn6.style.backgroundColor = "steelblue";
+    btn6.style.color = "white";
+    btn7.style.backgroundColor = "transparent";
+    btn7.style.color = "rgb(40,40,40)";
+    btn8.style.backgroundColor = "transparent";
+    btn8.style.color = "rgb(40,40,40)";
+}
+btn7.onclick = function() {
+    btn5.style.backgroundColor = "transparent";
+    btn5.style.color = "rgb(40,40,40)";
+    btn6.style.backgroundColor = "transparent";
+    btn6.style.color = "rgb(40,40,40)";
+    btn7.style.backgroundColor = "steelblue";
+    btn7.style.color = "white";
+    btn8.style.backgroundColor = "transparent";
+    btn8.style.color = "rgb(40,40,40)";
+}
+btn8.onclick = function() {
+    btn5.style.backgroundColor = "transparent";
+    btn5.style.color = "rgb(40,40,40)";
+    btn6.style.backgroundColor = "transparent";
+    btn6.style.color = "rgb(40,40,40)";
+    btn7.style.backgroundColor = "transparent";
+    btn7.style.color = "rgb(40,40,40)";
+    btn8.style.backgroundColor = "steelblue";
+    btn8.style.color = "white";
+}
+
+btn9.onclick = function() {
+    btn9.style.backgroundColor = "steelblue";
+    btn9.style.color = "white";
+    btn10.style.backgroundColor = "transparent";
+    btn10.style.color = "rgb(40,40,40)";
+    btn11.style.backgroundColor = "transparent";
+    btn11.style.color = "rgb(40,40,40)";
+    btn12.style.backgroundColor = "transparent";
+    btn12.style.color = "rgb(40,40,40)";
+}
+btn10.onclick = function() {
+    btn9.style.backgroundColor = "transparent";
+    btn9.style.color = "rgb(40,40,40)";
+    btn10.style.backgroundColor = "steelblue";
+    btn10.style.color = "white";
+    btn11.style.backgroundColor = "transparent";
+    btn11.style.color = "rgb(40,40,40)";
+    btn12.style.backgroundColor = "transparent";
+    btn12.style.color = "rgb(40,40,40)";
+}
+btn11.onclick = function() {
+    btn9.style.backgroundColor = "transparent";
+    btn9.style.color = "rgb(40,40,40)";
+    btn10.style.backgroundColor = "transparent";
+    btn10.style.color = "rgb(40,40,40)";
+    btn11.style.backgroundColor = "steelblue";
+    btn11.style.color = "white";
+    btn12.style.backgroundColor = "transparent";
+    btn12.style.color = "rgb(40,40,40)";
+}
+btn12.onclick = function() {
+    btn9.style.backgroundColor = "transparent";
+    btn9.style.color = "rgb(40,40,40)";
+    btn10.style.backgroundColor = "transparent";
+    btn10.style.color = "rgb(40,40,40)";
+    btn11.style.backgroundColor = "transparent";
+    btn11.style.color = "rgb(40,40,40)";
+    btn12.style.backgroundColor = "steelblue";
+    btn12.style.color = "white";
+}
+
+btn13.onclick = function() {
+    btn13.style.backgroundColor = "steelblue";
+    btn13.style.color = "white";
+    btn14.style.backgroundColor = "transparent";
+    btn14.style.color = "rgb(40,40,40)";
+    btn15.style.backgroundColor = "transparent";
+    btn15.style.color = "rgb(40,40,40)";
+    btn16.style.backgroundColor = "transparent";
+    btn16.style.color = "rgb(40,40,40)";
+}
+btn14.onclick = function() {
+    btn13.style.backgroundColor = "transparent";
+    btn13.style.color = "rgb(40,40,40)";
+    btn14.style.backgroundColor = "steelblue";
+    btn14.style.color = "white";
+    btn15.style.backgroundColor = "transparent";
+    btn15.style.color = "rgb(40,40,40)";
+    btn16.style.backgroundColor = "transparent";
+    btn16.style.color = "rgb(40,40,40)";
+}
+btn15.onclick = function() {
+    btn13.style.backgroundColor = "transparent";
+    btn13.style.color = "rgb(40,40,40)";
+    btn14.style.backgroundColor = "transparent";
+    btn14.style.color = "rgb(40,40,40)";
+    btn15.style.backgroundColor = "steelblue";
+    btn15.style.color = "white";
+    btn16.style.backgroundColor = "transparent";
+    btn16.style.color = "rgb(40,40,40)";
+}
+btn16.onclick = function() {
+    btn13.style.backgroundColor = "transparent";
+    btn13.style.color = "rgb(40,40,40)";
+    btn14.style.backgroundColor = "transparent";
+    btn14.style.color = "rgb(40,40,40)";
+    btn15.style.backgroundColor = "transparent";
+    btn15.style.color = "rgb(40,40,40)";
+    btn16.style.backgroundColor = "steelblue";
+    btn16.style.color = "white";
+}
+
+btn17.onclick = function() {
+    btn17.style.backgroundColor = "steelblue";
+    btn17.style.color = "white";
+    btn18.style.backgroundColor = "transparent";
+    btn18.style.color = "rgb(40,40,40)";
+    btn19.style.backgroundColor = "transparent";
+    btn19.style.color = "rgb(40,40,40)";
+    btn20.style.backgroundColor = "transparent";
+    btn20.style.color = "rgb(40,40,40)";
+}
+btn18.onclick = function() {
+    btn17.style.backgroundColor = "transparent";
+    btn17.style.color = "rgb(40,40,40)";
+    btn18.style.backgroundColor = "steelblue";
+    btn18.style.color = "white";
+    btn19.style.backgroundColor = "transparent";
+    btn19.style.color = "rgb(40,40,40)";
+    btn20.style.backgroundColor = "transparent";
+    btn20.style.color = "rgb(40,40,40)";
+}
+btn19.onclick = function() {
+    btn17.style.backgroundColor = "transparent";
+    btn17.style.color = "rgb(40,40,40)";
+    btn18.style.backgroundColor = "transparent";
+    btn18.style.color = "rgb(40,40,40)";
+    btn19.style.backgroundColor = "steelblue";
+    btn19.style.color = "white";
+    btn20.style.backgroundColor = "transparent";
+    btn20.style.color = "rgb(40,40,40)";
+}
+btn20.onclick = function() {
+    btn17.style.backgroundColor = "transparent";
+    btn17.style.color = "rgb(40,40,40)";
+    btn18.style.backgroundColor = "transparent";
+    btn18.style.color = "rgb(40,40,40)";
+    btn19.style.backgroundColor = "transparent";
+    btn19.style.color = "rgb(40,40,40)";
+    btn20.style.backgroundColor = "steelblue";
+    btn20.style.color = "white";
+}
